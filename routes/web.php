6c08652368fcc3a24ad0a5bb26420364add46d17
Agil Adefreditya <agiladefreditya@gmail.com>
2024-11-28 11:07:47 +0700

    AGENDA

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,11 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view ('welcome');
+});
+Route::get('/echo', function () {
+    echo 'Hello Pukimak!';
+});
+Route::get('/home', function () {
+    return view ('main');
 });
